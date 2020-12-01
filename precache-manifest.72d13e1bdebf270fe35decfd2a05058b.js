2f29d416a4a39bd5635e590418db056a41caf05e
Gerardo Gomez <Gerardo_andres272@hotmail.com>
2020-12-01 16:01:18 +0100

    Updates

---
+++ precache-manifest.72d13e1bdebf270fe35decfd2a05058b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f15ea41721de287a39b05899ffd2c0d",
+    "url": "/Fitness/index.html"
+  },
+  {
+    "revision": "137341b8abfe5f7daf10",
+    "url": "/Fitness/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "cc351c775f01ebae5d05",
+    "url": "/Fitness/static/css/main.043641db.chunk.css"
+  },
+  {
+    "revision": "137341b8abfe5f7daf10",
+    "url": "/Fitness/static/js/2.00492a15.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Fitness/static/js/2.00492a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc351c775f01ebae5d05",
+    "url": "/Fitness/static/js/main.37e2f211.chunk.js"
+  },
+  {
+    "revision": "22707c812eabc616009e",
+    "url": "/Fitness/static/js/runtime-main.406e4aec.js"
+  },
+  {
+    "revision": "f16a1ad5b4b9c86c0f30cd484308cb3b",
+    "url": "/Fitness/static/media/404.f16a1ad5.png"
+  },
+  {
+    "revision": "854842fa495e13de2d54968cec61b6d1",
+    "url": "/Fitness/static/media/500.854842fa.png"
+  },
+  {
+    "revision": "eb728687589f75caecdb7f2b3d71c1d5",
+    "url": "/Fitness/static/media/circles.eb728687.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Fitness/precache-manifest.72d13e1bdebf270fe35decfd2a05058b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fitness/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
